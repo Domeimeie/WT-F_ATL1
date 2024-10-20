f4389b166b22adab74ba65731c99b08c24e19a2b
Dominique <schlurpf@gmail.com>
2024-10-20 12:19:59 +0200

    Implemented index view

--- index.php
+++ index.php
@@ -14,10 +14,28 @@
 </head>
 
 <body>
-  <?php include('navigation.php'); ?>
-  <div class="content">
-  </div>
-  <?php include('footer.html'); ?>
+  <?php include'navigation.php'; ?>
+  <main class="content">
+    <article>
+      <div class="welcomeDesc">
+        <h1>Project Dodo Revival:<br> Bringing an Icon Back to Life</h1>
+        <p>
+          Project Dodo Revival is a groundbreaking conservation and genetic engineering initiative aimed at bringing the iconic 
+          dodo bird back from extinction. Utilizing cutting-edge advancements in DNA research and cloning, scientists are working 
+          to reconstruct the dodo’s genetic code using preserved DNA from dodo remains and closely related bird species. The 
+          project not only seeks to restore the dodo to its natural habitat in Mauritius but also raises awareness about 
+          biodiversity loss and the importance of protecting endangered species today. Through this initiative, the dodo could once 
+          again roam the Earth, symbolizing hope for the future of conservation.<br>
+          Is this something you support? Feel free to check out our merch store to support us financially.
+        </p>
+        <a href="product.php" class="support">Support our cause</a>
+      </div>
+      <picture class="welcomePicture">
+          <img src="media/dodoIndex.png" alt="Historical Dodo">
+      </picture>
+    </article>
+  </main>
+  <?php include'footer.html'; ?>
 </body>
 
 </html>
